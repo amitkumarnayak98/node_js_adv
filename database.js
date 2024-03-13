a93766bf6f4f608443f6354e2f89414499acb8b9
amitkumarnayak98 <amitkunayak567890@gmail.com>
2024-03-13 23:35:35 +0530

    feat(NODE_PROG): Updated the code base for the node project

---
+++ database.js
@@ -0,0 +1,22 @@
+const { createPool } = require("mysql");
+
+const pool = createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test",
+  connectionLimit: 10,
+});
+
+pool.query(
+  `select * from registration`,
+  // [3],
+  (err, result, fields) => {
+    if (err) {
+      return console.log(err);
+    }
+    return console.log(result);
+  }
+);
+
+module.exports = pool;
